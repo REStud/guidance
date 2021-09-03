caa999bc6751ec891485a48bfd2aae785bd29e7f
Miklos Koren <miklos.koren@gmail.com>
2021-09-03 17:17:20 +0200

    Render report template

--- DAP/render.py
+++ DAP/render.py
@@ -5,9 +5,20 @@ from yaml import load
 file_name = 'Report'
 version = '1.0'
 
+def render_list(lst):
+    return '\n\r\n\r'.join(['({}) {}'.format(chr(ord('a') + i), item) for i, item in enumerate(lst)])
+
+def apply(item):
+    if isinstance(item, list):
+        return render_list(item)
+    if isinstance(item, dict):
+        return {key: apply(item[key]) for key in item}
+    return item
+
 doc = DocxTemplate(f'{file_name}.docx')
 #md = Template(open('syllabus-template.md', 'rt').read())
-context = load(open(f'{file_name}-{version}.yaml'))
+context = apply(load(open(f'{file_name}-{version}.yaml')))
+context['version'] = version
 
 # save .md
 #with open('{}.md'.format(file_name), 'wt') as file:
