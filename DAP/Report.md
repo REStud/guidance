b6fbe57edf98b86807206df2f5562d9fa0fa7d8d
Miklos Koren <miklos.koren@gmail.com>
2021-09-03 17:24:32 +0200

    Render .md format

---
+++ DAP/Report.md
@@ -0,0 +1,19 @@
+# Replication Package Reporting Standard v{{ version }}
+## What to include in the README file
+
+| | Item No | Rule | Page or Line No |
+|---|---|---|---|
+| **Citation** |
+| Data| 1 | {{ cite.data }} | |
+| Software | 2 | {{ cite.software }} | |
+| **Dependencies** |
+| Data | 3 |{{ dependency.data }} | |
+| Software | 4 | {{ dependency.software }} | |
+| Environment | 5 | {{ dependency.environment }} | |
+| **Guidance** |
+| Instructions | 6 | {{ guide.instruction }} | |
+| Data list | 7 | {{ guide.data }} | |
+| Exhibit list | 8 | {{ guide.exhibit }} | |
+| **The README document** |
+| Format | 9 | {{ readme.format }} | |
+
--- DAP/render.py
+++ DAP/render.py
@@ -2,11 +2,10 @@ from jinja2 import Template
 from docxtpl import DocxTemplate
 from yaml import load
 
-file_name = 'Report'
 version = '1.0'
 
 def render_list(lst):
-    return '\n\r\n\r'.join(['({}) {}'.format(chr(ord('a') + i), item) for i, item in enumerate(lst)])
+    return ' '.join(['({}) {}'.format(chr(ord('a') + i), item) for i, item in enumerate(lst)])
 
 def apply(item):
     if isinstance(item, list):
@@ -15,15 +14,16 @@ def apply(item):
         return {key: apply(item[key]) for key in item}
     return item
 
-doc = DocxTemplate(f'{file_name}.docx')
-#md = Template(open('syllabus-template.md', 'rt').read())
-context = apply(load(open(f'{file_name}-{version}.yaml')))
-context['version'] = version
+for file_name in ['Report']:
+    doc = DocxTemplate(f'{file_name}.docx')
+    md = Template(open(f'{file_name}.md', 'rt').read())
+    context = apply(load(open(f'{file_name}-{version}.yaml')))
+    context['version'] = version
 
-# save .md
-#with open('{}.md'.format(file_name), 'wt') as file:
-#	file.write(md.render(context))
+    # save .md
+    with open(f'{file_name}-{version}.md', 'wt') as file:
+    	file.write(md.render(context))
 
-# then .docx
-doc.render(context)
-doc.save(f'{file_name}-{version}.docx')
+    # then .docx
+    doc.render(context)
+    doc.save(f'{file_name}-{version}.docx')
